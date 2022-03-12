76b11a30886b76e7dba5a1c10da09a1607459c6e
Jose <joseoctavioconti@gmail.com>
2022-03-12 16:50:24 -0300

    Update tecnology done

---
+++ src/modules/tecnologies/services/UpdateTecnologyService.ts
@@ -0,0 +1,29 @@
+import { injectable, inject } from 'tsyringe';
+import AppError from '@shared/errors/AppError';
+import Tecnology from '@modules/tecnologies/infra/typeorm/entities/Tecnology';
+import ITecnologiesRepository from '../repositories/ITecnologiesRepository';
+
+interface IRequest {
+  tecnology_id: string;
+  name: string;
+}
+
+@injectable()
+class UpdateTecnologyServicd {
+  constructor(
+    @inject('TecnologiesRepository')
+    private tecnologiesRepository: ITecnologiesRepository,
+  ) {}
+
+  public async execute({ tecnology_id, name }: IRequest): Promise<Tecnology> {
+    const tecnology = await this.tecnologiesRepository.findById(tecnology_id);
+
+    tecnology.name = name;
+
+    await this.tecnologiesRepository.update(tecnology);
+
+    return tecnology;
+  }
+}
+
+export default UpdateTecnologyServicd;
