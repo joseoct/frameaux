3c8ee8c4e0fd07379a9704344ba65240767935ca
Jose <joseoctavioconti@gmail.com>
2022-03-12 22:11:18 -0300

    Roles modifications

---
+++ src/modules/roles/infra/prisma/repositories/RolesRepository.ts
@@ -0,0 +1,28 @@
+import IRolesRepository from '@modules/roles/repositories/IRolesRepository';
+import ICreateRoleDTO from '@modules/roles/dtos/ICreateRoleDTO';
+
+import { PrismaClient, Role } from '@prisma/client';
+
+class RoleRepository implements IRolesRepository {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  public async findRoleByName(name: string): Promise<Role | undefined> {
+    const role = await this.prisma.role.findUnique({
+      where: { name },
+    });
+
+    return role;
+  }
+
+  public async create(roleData: ICreateRoleDTO): Promise<Role> {
+    const role = this.prisma.role.create({ data: roleData });
+
+    return role;
+  }
+}
+
+export default RoleRepository;
--- src/modules/roles/repositories/IRolesRepository.ts
+++ src/modules/roles/repositories/IRolesRepository.ts
@@ -1,8 +1,7 @@
-import Role from '../infra/typeorm/entities/Role';
+import { Role } from '@prisma/client';
 import ICreateRoleDTO from '../dtos/ICreateRoleDTO';
 
 export default interface IUsersRepository {
   findRoleByName(name: string): Promise<Role | undefined>;
   create(data: ICreateRoleDTO): Promise<Role>;
-  save(data: Role): Promise<Role>;
 }
