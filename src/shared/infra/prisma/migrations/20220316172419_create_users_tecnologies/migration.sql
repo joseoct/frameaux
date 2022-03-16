e929c7aa9bfedc693ba87988c95bb9fbd4883d26
José Octávio <joseoctavioconti@gmail.com>
2022-03-16 14:31:54 -0300

    Scheme of users_tecnologies updated

--- src/shared/infra/prisma/migrations/20220316172419_create_users_tecnologies/migration.sql
+++ src/shared/infra/prisma/migrations/20220316172419_create_users_tecnologies/migration.sql
@@ -3,7 +3,8 @@ CREATE TABLE "users_tecnologies" (
     "id" TEXT NOT NULL,
     "user_id" TEXT NOT NULL,
     "tecnology_id" TEXT NOT NULL,
-    "current_layer" INT NOT NULL,
+    "current_layer" INTEGER NOT NULL,
+    "role_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -12,6 +13,9 @@ CREATE TABLE "users_tecnologies" (
 
 -- AddForeignKey
 ALTER TABLE "users_tecnologies" ADD CONSTRAINT "users_tecnologies_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users_tecnologies" ADD CONSTRAINT "users_tecnologies_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "users_tecnologies" ADD CONSTRAINT "users_tecnologies_tecnology_id_fkey" FOREIGN KEY ("tecnology_id") REFERENCES "tecnologies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
